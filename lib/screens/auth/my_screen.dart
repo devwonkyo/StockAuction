3213efa223f676fb0c340c1a2424854f673e0416
[APP1] 정원교 <devwonkyo@naver.com>
2024-09-20 14:04:19 +0900

    test

--- lib/screens/auth/my_screen.dart
+++ lib/screens/auth/my_screen.dart
@@ -8,6 +8,6 @@ class MyScreen extends StatefulWidget {
 class _MyScreenState extends State<MyScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Colum();
   }
 }
