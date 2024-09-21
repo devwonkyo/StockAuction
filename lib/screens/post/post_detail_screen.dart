ffe5b586cf622b6b84f0eee3bb099295f5e9eb4f
[APP1] 정원교 <dnjsry159@naver.com>
2024-09-21 17:05:29 +0900

    postDetail ui 작업중 , class name 변경

--- lib/screens/post/post_detail_screen.dart
+++ lib/screens/post/post_detail_screen.dart
@@ -1,12 +1,16 @@
+import 'package:auction/screens/post/widgets/favorite_button_widget.dart';
+import 'package:auction/screens/post/widgets/timer_text_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
+import 'package:provider/provider.dart';
 
-class PostDetailPage extends StatefulWidget {
+
+class PostDetailScreen extends StatefulWidget {
   @override
-  _PostDetailPageState createState() => _PostDetailPageState();
+  _PostDetailScreenState createState() => _PostDetailScreenState();
 }
 
-class _PostDetailPageState extends State<PostDetailPage> {
+class _PostDetailScreenState extends State<PostDetailScreen> {
   int _selectedIndex = 0;
 
   // 네비게이터의 아이템 선택 시 처리 함수
@@ -31,6 +35,14 @@ class _PostDetailPageState extends State<PostDetailPage> {
             Navigator.of(context).pop();
           },
         ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.more_vert_outlined),
+            onPressed: () {
+              _showOptionsBottomSheet(context);
+            },
+          ),
+        ],
       ),
       body: Column(
         children: [
@@ -55,16 +67,23 @@ class _PostDetailPageState extends State<PostDetailPage> {
                   ),
                   // 제품 정보 부분
                   Padding(
-                    padding: const EdgeInsets.all(16.0),
+                    padding: const EdgeInsets.all(20.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          'Nike x Sacai VaporWaffle Dark Iris',
-                          style: TextStyle(
-                              fontSize: 22, fontWeight: FontWeight.bold),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                'Nike x Sacai VaporWaffle Dark Iris',
+                                style: TextStyle(
+                                    fontSize: 24),
+                              ),
+                            ),
+                            FavoriteButtonWidget(isFavorited: false,)
+                          ],
                         ),
-                        SizedBox(height: 8),
+                        SizedBox(height: 15),
                         Text(
                           '조금은 긴 글내용/Users/wonkyo/Documents/elice/Auction/aution/lib/screens/post/post_detail_screen.dart조금은 긴 글내용/Users/wonkyo/Documents/elice/Auction/aution/lib/screens/post/post_detail_screen.dart'
                           '조금은 긴 글내용/Users/wonkyo/Documents/elice/Auction/aution/lib/screens/post/post_detail_screen.dart'
@@ -74,8 +93,27 @@ class _PostDetailPageState extends State<PostDetailPage> {
                           'ㅍ'
                           '조금은 긴 글내용/Users/wonkyo/Documents/elice/Auction/aution/lib/screens/post/post_detail_screen.dart',
                           style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
+                              fontSize: 15),
                         ),
+                        SizedBox(height: 30.0,),
+                        Divider(height: 1, color: Colors.grey,),
+                        SizedBox(height: 30.0,),
+                        Text(
+                          '댓글 3',
+                          style: TextStyle(
+                              fontSize: 14),
+                        ),
+                        ListView.builder(
+                          itemCount: 3,
+                          itemBuilder:(context, index){
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+
+                              ],
+                            );
+                          },
+                        )
                       ],
                     ),
                   ),
@@ -90,7 +128,13 @@ class _PostDetailPageState extends State<PostDetailPage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('최근 거래가', style: TextStyle(fontSize: 16)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('현재 입찰가', style: TextStyle(fontSize: 16)),
+                      TimerTextWidget(time: 530,),
+                    ],
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -107,21 +151,24 @@ class _PostDetailPageState extends State<PostDetailPage> {
                     children: [
                       Expanded(
                         child: TextField(
+                            keyboardType: TextInputType.number,
                             decoration: InputDecoration(
                           border: OutlineInputBorder(),
                           labelText: '가격을 입력해주세요',
                         )),
                       ),
-                      const SizedBox(width: 10,),
+                      const SizedBox(width: 20,),
                       ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () {
+
+                        },
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Color(0xFF65AE7E),
                           padding: EdgeInsets.symmetric(
-                              horizontal: 32, vertical: 12),
+                              horizontal: 20, vertical: 12),
                         ),
                         child: Text(
-                          '입찰',
+                          '입찰하기',
                           style: TextStyle(color: Colors.white),
                         ),
                       ),
@@ -135,4 +182,59 @@ class _PostDetailPageState extends State<PostDetailPage> {
       ),
     );
   }
+
+  void _showOptionsBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: Icon(Icons.edit),
+                title: Text('수정'),
+                onTap: () {
+                  Navigator.of(context).pop(); // 시트 닫기
+                  _editItem(context); // 수정 처리
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.delete),
+                title: Text('삭제'),
+                onTap: () {
+                  Navigator.of(context).pop(); // 시트 닫기
+                  _deleteItem(context); // 삭제 처리
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.cancel),
+                title: Text('취소'),
+                onTap: () {
+                  Navigator.of(context).pop(); // 시트 닫기
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _editItem(BuildContext context) {
+    // 수정 처리 로직
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('수정 기능이 호출되었습니다.')),
+    );
+  }
+
+  void _deleteItem(BuildContext context) {
+    // 삭제 처리 로직
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('삭제 기능이 호출되었습니다.')),
+    );
+  }
 }
+
+
