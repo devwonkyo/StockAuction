7c63dc582e39500906ba179e733f60228de85e10
SweetFriedPotato <123169135+SweetFriedPotato@users.noreply.github.com>
2024-09-23 13:50:43 +0900

    mypage go route

---
+++ lib/screens/my/my_sold_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MySoldScreen extends StatelessWidget {
+  const MySoldScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('MySoldScreen')),
+      body: const Center(child: Text('MySoldScreen')),
+    );
+  }
+}
