87d200933a58c8f974afa9201c8b5e15b7b36f07
[APP1] 조정현 <abeul25@naver.com>
2024-09-23 09:04:34 +0000

    route.dart 버그 수정 및 절대경로로 변경

--- lib/route.dart
+++ lib/route.dart
@@ -1,28 +1,31 @@
-import 'package:auction/screens/post/bid_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:auction/screens/main_screen.dart';
+// import auth
 import 'package:auction/screens/auth/login_screen.dart';
 import 'package:auction/screens/auth/signup_screen.dart';
 import 'package:auction/screens/auth/password_reser_screen.dart';
 import 'package:auction/screens/auth/settings_screen.dart';
 import 'package:auction/screens/auth/my_screen.dart';
+// import chat
 import 'package:auction/screens/chat/chat_list_screen.dart';
 import 'package:auction/screens/chat/chat_screen.dart';
+// import post
 import 'package:auction/screens/post/post_list_screen.dart';
 import 'package:auction/screens/post/post_add_screen.dart';
 import 'package:auction/screens/post/post_detail_screen.dart';
-import 'package:auction/screens/main_screen.dart';
+import 'package:auction/screens/post/bid_list_screen.dart';
+// import my
+import 'package:auction/screens/my/my_sold_screen.dart';
+import 'package:auction/screens/my/my_account_screen.dart';
+import 'package:auction/screens/my/my_bought_screen.dart';
+import 'package:auction/screens/my/my_deliver_screen.dart';
+import 'package:auction/screens/my/my_infoupdate_screen.dart';
 
 
 // 보통 아래와 같은 방식으로 이동 가능합니다
 // GoRouter.of(context).go('/example');
 // GoRouter.of(context).push('/example');
-
-import 'screens/my/my_sold_screen.dart';
-import 'screens/my/my_account_screen.dart';
-import 'screens/my/my_bought_screen.dart';
-import 'screens/my/my_deliver_screen.dart';
-import 'screens/my/my_infoupdate_screen.dart';
 
 final rootNavigatorKey = GlobalKey<NavigatorState>();
 
@@ -52,7 +55,6 @@ final GoRouter router = GoRouter(
     ),
     GoRoute(
       path: '/post/detail',
-      builder: (context, state) => PostDetailScreen(),
       builder: (context, state) => PostDetailScreen(),
     ),
     GoRoute(
