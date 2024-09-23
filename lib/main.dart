8fe86afd0d9acbae9ae5f5ead99143d87db8c664
[APP1] 정원교 <dnjsry159@naver.com>
2024-09-23 17:20:07 +0900

    경매리스트 UI작업중...

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,9 @@
-import 'package:auction/config/theme.dart';
-import 'package:auction/screens/auth/login_screen.dart';
-import 'package:auction/screens/main_screen.dart';
+import 'package:auction/providers/post_provider.dart';
+import 'package:auction/providers/theme_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:auction/screens/auth/login_screen.dart';
-import 'package:auction/screens/auth/settings_screen.dart';
-import 'package:auction/screens/auth/my_screen.dart';
-import 'package:auction/screens/chat/chat_list_screen.dart';
-import 'package:auction/screens/post/post_list_screen.dart';
-import 'package:auction/screens/main_screen.dart';
 import 'package:auction/route.dart';
-import 'dart:async';
-//firebase 패키지
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -36,6 +26,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      theme: Provider.of<ThemeProvider>(context).currentTheme, //todo toggle 시 Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+      // theme: darkThemeData(),
       routerDelegate: router.routerDelegate,
       routeInformationParser: router.routeInformationParser,
       routeInformationProvider: router.routeInformationProvider,
