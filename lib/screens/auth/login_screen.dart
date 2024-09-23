efeea1574b8e3d9757456c656472f1b78de92248
[APP1] 고규화 <rhrbghk@naver.com>
2024-09-23 14:08:10 +0900

    이메일 인증, Auth부분 GoRouter 적용 완료

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,40 +1,27 @@
-import 'package:auction/screens/auth/password_reser_screen.dart';
-import 'package:auction/screens/auth/signup_screen.dart';
-import 'package:auction/screens/main_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:auction/providers/auth_provider.dart';
 
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
+class LoginScreen extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
-  String _email = '';
-  String _password = '';
-  bool _stayLoggedIn = false;
-
-  void _login() async {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-      try {
-        await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: _email,
-          password: _password,
-        );
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => MainScreen()),
-        );
-      } catch (e) {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('아이디 또는 비밀번호를 확인해주세요')));
-      }
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+
+    void _login() async {
+      if (_formKey.currentState!.validate()) {
+        _formKey.currentState!.save();
+        try {
+          await authProvider.login();
+          context.go('/main');
+        } catch (e) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('아이디 또는 비밀번호를 확인해주세요')));
+        }
+      }
+    }
+
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.fromLTRB(20, 200, 20, 0),
@@ -46,14 +33,12 @@ class _LoginScreenState extends State<LoginScreen> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Text(
-                    '환영합니다.',
+                    'stockAuction.',
                     style: TextStyle(fontSize: 50, fontWeight: FontWeight.w300),
                   ),
                 ],
               ),
-              SizedBox(
-                height: 100,
-              ),
+              SizedBox(height: 100),
               TextFormField(
                 decoration: InputDecoration(
                   labelText: '아이디를 입력해 주세요.',
@@ -62,11 +47,9 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
                 validator: (value) => value!.isEmpty ? '아이디를 입력해주세요.' : null,
-                onSaved: (value) => _email = value!,
+                onSaved: (value) => authProvider.setEmail(value!),
               ),
-              SizedBox(
-                height: 30,
-              ),
+              SizedBox(height: 20),
               TextFormField(
                 decoration: InputDecoration(
                   labelText: '비밀번호를 입력해 주세요.',
@@ -76,42 +59,32 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
                 obscureText: true,
                 validator: (value) => value!.isEmpty ? '비밀번호를 입력해주세요.' : null,
-                onSaved: (value) => _password = value!,
+                onSaved: (value) => authProvider.setPassword(value!),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => PasswordResetScreen()),
-                      );
-                    },
+                    onTap: () => context.push('/password-reset'),
                     child: Text(
                       '암호를 잊어버렸습니다.',
-                      style: TextStyle(
-                        color: Colors.red,
-                      ),
+                      style: TextStyle(color: Colors.red),
                     ),
                   ),
                 ],
               ),
-              SizedBox(
-                height: 30,
-              ),
+              SizedBox(height: 30),
               ElevatedButton(
                 child: Text(
                   '로그인',
-                  style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600),
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
                 ),
                 onPressed: _login,
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Color(0XFFEBCDFC),
                   minimumSize: Size(double.infinity, 50),
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0), // 둥근 모서리 설정
+                    borderRadius: BorderRadius.circular(15.0),
                   ),
                 ),
               ),
@@ -119,20 +92,12 @@ class _LoginScreenState extends State<LoginScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        _stayLoggedIn = !_stayLoggedIn; // 체크박스 상태 변경
-                      });
-                    },
+                    onTap: () => authProvider.setStayLoggedIn(!authProvider.stayLoggedIn),
                     child: Text('자동 로그인'),
                   ),
                   Checkbox(
-                    value: _stayLoggedIn,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        _stayLoggedIn = value ?? false; // 체크박스 상태 업데이트
-                      });
-                    },
+                    value: authProvider.stayLoggedIn,
+                    onChanged: (bool? value) => authProvider.setStayLoggedIn(value ?? false),
                   ),
                 ],
               ),
@@ -141,17 +106,10 @@ class _LoginScreenState extends State<LoginScreen> {
                 children: [
                   Text('계정이 없습니다.'),
                   GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SignupScreen()),
-                      );
-                    },
+                    onTap: () => context.push('/signup'),
                     child: Text(
                       '회원가입',
-                      style: TextStyle(
-                        color: Colors.red,
-                      ),
+                      style: TextStyle(color: Colors.red),
                     ),
                   ),
                 ],
