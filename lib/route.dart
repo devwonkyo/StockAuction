0966a841c0a0607c4890bb14643edac8ae118f60
JeongHyeon Jo <abeul25@naver.com>
2024-09-23 16:57:54 +0900

    실행해본적 없어용 ㅎㅎ

---
+++ lib/route.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:auction/screens/auth/login_screen.dart';
+import 'package:auction/screens/auth/settings_screen.dart';
+import 'package:auction/screens/auth/my_screen.dart';
+import 'package:auction/screens/chat/chat_list_screen.dart';
+import 'package:auction/screens/chat/chat_screen.dart';
+import 'package:auction/screens/post/post_list_screen.dart';
+import 'package:auction/screens/post/post_add_screen.dart';
+import 'package:auction/screens/post/post_detail_screen.dart';
+import 'package:auction/screens/main_screen.dart';
+
+// 보통 아래와 같은 방식으로 이동 가능합니다
+// GoRouter.of(context).go('/example');
+// GoRouter.of(context).push('/example');
+
+final rootNavigatorKey = GlobalKey<NavigatorState>();
+
+final GoRouter router = GoRouter(
+  initialLocation: '/main',
+  navigatorKey: rootNavigatorKey,
+  routes: [
+    GoRoute(
+      path: '/main',
+      builder: (context, state) => MainScreen(),
+    ),
+    GoRoute(
+      path: '/post/add',
+      builder: (context, state) => PostAddScreen(),
+    ),
+    GoRoute(
+      path: '/post/detail',
+      builder: (context, state) => PostDetailScreen(),
+    ),
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => LoginScreen(),
+    ),
+    GoRoute(
+      path: '/chat/:chatId',
+      builder: (BuildContext context, GoRouterState state) {
+        final chatId = state.pathParameters['chatId']!;
+        return ChatScreen(chatId: chatId);
+      },
+    ),
+  ],
+);
---
+++ lib/screens/auth/signup_screen.dart
@@ -0,0 +1,176 @@
+import 'package:auction/screens/auth/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:auction/models/user_model.dart'; // UserModel import 추가
+
+class SignupScreen extends StatefulWidget {
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _email = '';
+  String _password = '';
+  String _confirmPassword = '';
+  String _nickname = '';
+  String _phoneNumber = '';
+
+  Future<bool> _checkNickname() async {
+    var snapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .where('nickname', isEqualTo: _nickname)
+        .get();
+
+    return snapshot.docs.isEmpty;
+  }
+
+  void _signup() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      if (_password != _confirmPassword) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('비밀번호가 일치하지 않습니다.')),
+        );
+        return;
+      }
+
+      try {
+        // 닉네임 중복 체크
+        bool isNicknameAvailable = await _checkNickname();
+        if (!isNicknameAvailable) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('이미 사용 중인 닉네임입니다.')),
+          );
+          return;
+        }
+
+        // 사용자 생성
+        UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: _email,
+          password: _password,
+        );
+
+        // UserModel 인스턴스 생성
+        UserModel newUser = UserModel(
+          uid: userCredential.user!.uid,
+          email: _email,
+          nickname: _nickname,
+          phoneNumber: _phoneNumber,
+          pushToken: null,
+          userProfileImage: null,
+        );
+
+        // Firestore에 사용자 정보 저장
+        await FirebaseFirestore.instance.collection('users').doc(userCredential.user!.uid).set(newUser.toMap());
+
+        print('User created successfully: ${userCredential.user!.uid}');
+        print('User data saved to Firestore');
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('회원가입이 완료되었습니다.')),
+        );
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LoginScreen()),
+        );
+      } catch (e) {
+        print('Signup Error: $e');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('회원가입에 실패했습니다: $e')),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('회원가입')),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: '이메일 주소를 입력해주세요',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+                validator: (value) => value!.isEmpty ? '이메일을 입력해주세요' : null,
+                onSaved: (value) => _email = value!,
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: '닉네임',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+                validator: (value) => value!.isEmpty ? '닉네임을 입력해주세요' : null,
+                onSaved: (value) => _nickname = value!,
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: '비밀번호',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+                obscureText: true,
+                validator: (value) => value!.isEmpty ? '비밀번호를 입력해주세요' : null,
+                onSaved: (value) => _password = value!,
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: '비밀번호 확인',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+                obscureText: true,
+                validator: (value) => value!.isEmpty ? '비밀번호를 다시 입력해주세요' : null,
+                onSaved: (value) => _confirmPassword = value!,
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: '전화번호',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+                validator: (value) => value!.isEmpty ? '전화번호를 입력해주세요' : null,
+                onSaved: (value) => _phoneNumber = value!,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _signup,
+                child: Text('회원가입'),
+                style: ElevatedButton.styleFrom(
+                  textStyle: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 150, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  backgroundColor: Color(0XFFEBCDFC),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
