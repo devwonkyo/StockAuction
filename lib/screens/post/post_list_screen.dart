ae7f3a78821a626e6c6b430c2978f74403ddaa27
[APP1] 정원교 <dnjsry159@naver.com>
2024-09-24 20:52:27 +0900

    postListScreen 데이터 연동 완료

--- lib/screens/post/post_list_screen.dart
+++ lib/screens/post/post_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:auction/config/color.dart';
+import 'package:auction/models/post_model.dart';
+import 'package:auction/models/result_model.dart';
 import 'package:auction/providers/post_provider.dart';
 import 'package:auction/route.dart';
 import 'package:auction/screens/post/widgets/post_item_widget.dart';
@@ -13,7 +15,20 @@ class PostListScreen extends StatefulWidget {
   State<PostListScreen> createState() => _PostListScreenState();
 }
 
+
 class _PostListScreenState extends State<PostListScreen> {
+  late final result;
+  @override
+  void initState() {
+    super.initState();
+    _fetchPosts();
+  }
+
+  Future<void> _fetchPosts() async {
+    final postProvider = Provider.of<PostProvider>(context, listen: false);
+    result = await postProvider.getAllPostList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<PostProvider>(
@@ -23,14 +38,17 @@ class _PostListScreenState extends State<PostListScreen> {
           body: Padding(
             padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
             child: RefreshIndicator(
-                onRefresh: () => postProvider.refreshItems(),
-                child: MasonryGridView.count(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 20,
-                  crossAxisSpacing: 10,
-                  itemCount: 9,
+                onRefresh: () => postProvider.getAllPostList(),
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // Number of columns
+                    mainAxisSpacing: 20, // Vertical spacing
+                    crossAxisSpacing: 10, // Horizontal spacing
+                    childAspectRatio: 1/2, // Adjust aspect ratio to fit your needs
+                  ),
+                  itemCount: postProvider.postList.length,
                   itemBuilder: (BuildContext context, int index) {
-                    return PostItemWidget();
+                    return PostItemWidget(postModel: postProvider.postList[index]);
                   },
                 ),
             ),
--- lib/screens/post/widgets/post_item_widget.dart
+++ lib/screens/post/widgets/post_item_widget.dart
@@ -1,11 +1,14 @@
 import 'package:auction/config/color.dart';
+import 'package:auction/models/post_model.dart';
 import 'package:auction/screens/post/widgets/favorite_button_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 
 class PostItemWidget extends StatelessWidget {
-  const PostItemWidget({super.key});
+  final PostModel postModel;
+
+  const PostItemWidget({super.key, required this.postModel});
 
   @override
   Widget build(BuildContext context) {
@@ -18,29 +21,31 @@ class PostItemWidget extends StatelessWidget {
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
-                child: Image.network("https://via.placeholder.com/60",
-                  width: double.infinity,
-                  fit: BoxFit.cover,),
+              AspectRatio(
+                aspectRatio: 1,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.network(postModel.postImageList[0],
+                    fit: BoxFit.cover,),
+                ),
               ),
-              SizedBox(height: 10,),
-              Text("Title",style: TextStyle(fontSize: 16,
+              const SizedBox(height: 10,),
+              Text(postModel.postTitle,style: const TextStyle(fontSize: 16,
                   fontWeight: FontWeight.bold),),
-              SizedBox(height: 10),
-              Text("discriptiondiscriptiondiscriptiondiscriptiondiscriptiondiscription",
+              const SizedBox(height: 10),
+              Text(postModel.postContent,
                 style: TextStyle(fontSize: 14),
                 overflow: TextOverflow.ellipsis,
                 maxLines: 2,),
-              SizedBox(height: 20,),
-              Text("999,999원",style: TextStyle(fontSize: 18 ,fontWeight: FontWeight.bold),),
-              Text("현재 입찰가",style: TextStyle(fontSize: 12,color: AppsColor.darkGray),),
-              SizedBox(height: 5,),
+              const SizedBox(height: 20,),
+              Text(postModel.priceList.last,style: const TextStyle(fontSize: 18 ,fontWeight: FontWeight.bold),),
+              const Text("현재 입찰가",style: TextStyle(fontSize: 12,color: AppsColor.darkGray),),
+              const SizedBox(height: 5,),
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Icon(Icons.article_outlined,size: 14,),
-                  Text("13",style: TextStyle(fontSize: 14),)
+                  const Icon(Icons.article_outlined,size: 14,),
+                  Text(postModel.commentList.length.toString(),style: const TextStyle(fontSize: 14),)
                 ],
               )
             ],
