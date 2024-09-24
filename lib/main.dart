1358d614939dbf19684cbf9e296af59e96ea08be
SweetFriedPotato <123169135+SweetFriedPotato@users.noreply.github.com>
2024-09-24 18:19:56 +0900

    마이페이지 / 권한 수정

--- lib/main.dart
+++ lib/main.dart
@@ -8,34 +8,40 @@ import 'package:auction/providers/post_provider.dart';
 import 'package:auction/providers/theme_provider.dart';
 import 'package:auction/providers/chat_provider.dart';
 import 'package:auction/providers/auth_provider.dart';
+import 'package:auction/providers/my_provider.dart';
 // firebase 패키지
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();  // Firebase 초기화
+  await Firebase.initializeApp(); // Firebase 초기화
 
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => ThemeProvider()), //테마 변경 Provider
-        ChangeNotifierProvider(create: (context) => PostProvider()), //포스트 Provider
-        ChangeNotifierProvider(create: (context) => ChatProvider()), // 채팅 Provider
+        ChangeNotifierProvider(
+            create: (context) => ThemeProvider()), //테마 변경 Provider
+        ChangeNotifierProvider(
+            create: (context) => PostProvider()), //포스트 Provider
+        ChangeNotifierProvider(
+            create: (context) => ChatProvider()), // 채팅 Provider
         ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(
+          create: (context) => MyProvider(),
+        ),
       ],
       child: MyApp(),
     ),
   );
 }
 
-
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      theme: Provider.of<ThemeProvider>(context).currentTheme, //todo toggle 시 Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+      theme: Provider.of<ThemeProvider>(context)
+          .currentTheme, //todo toggle 시 Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
       // theme: darkThemeData(),
       routerDelegate: router.routerDelegate,
       routeInformationParser: router.routeInformationParser,
@@ -43,5 +49,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
