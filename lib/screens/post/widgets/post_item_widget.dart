ca19e8338b9e3117013a1205f016a07c40b2676b
[APP1] 고규화 <rhrbghk@naver.com>
2024-09-26 11:16:44 +0900

    MyLikesScreen

--- lib/screens/post/widgets/post_item_widget.dart
+++ lib/screens/post/widgets/post_item_widget.dart
@@ -3,7 +3,6 @@ import 'package:auction/models/post_model.dart';
 import 'package:auction/screens/post/widgets/favorite_button_widget.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 
 class PostItemWidget extends StatelessWidget {
@@ -14,59 +13,74 @@ class PostItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         print('postitemwidget postUid : ${postModel.postUid}');
         context.push("/post/detail", extra: postModel.postUid);
       },
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          AspectRatio(
-            aspectRatio: 1,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: CachedNetworkImage(
-                  imageUrl: postModel.postImageList[0],
-                  errorWidget: (context, url, error) => const Icon(Icons.error),
-                  fit: BoxFit.cover),
+      child: Container(
+        height: 396, // 고정 높이 설정
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: CachedNetworkImage(
+                    imageUrl: postModel.postImageList[0],
+                    errorWidget: (context, url, error) => const Icon(Icons.error),
+                    fit: BoxFit.cover),
+              ),
             ),
-          ),
-          const SizedBox(height: 10,),
-          Text(postModel.postTitle,style: const TextStyle(fontSize: 16,
-              fontWeight: FontWeight.bold),),
-          const SizedBox(height: 10),
-          Text(postModel.postContent,
-            style: TextStyle(fontSize: 14),
-            overflow: TextOverflow.ellipsis,
-            maxLines: 2,),
-          const SizedBox(height: 20,),
-          Text(postModel.priceList.last,style: const TextStyle(fontSize: 18 ,fontWeight: FontWeight.bold),),
-          const Text("현재 입찰가",style: TextStyle(fontSize: 12,color: AppsColor.darkGray),),
-          const SizedBox(height: 5,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              GestureDetector(
-                onTap: (){
-                  print('click comment');
-                },
-                child: Row(
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    FavoriteButtonWidget(isFavorited: false,size: 16, onPressed: () {  },),
-                    Text(postModel.favoriteList.length.toString(),style: const TextStyle(fontSize: 16),)
+                    Text(
+                      postModel.postTitle,
+                      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      postModel.postContent,
+                      style: TextStyle(fontSize: 14),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const Spacer(),
+                    Text(
+                      postModel.priceList.last,
+                      style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const Text("현재 입찰가", style: TextStyle(fontSize: 12, color: AppsColor.darkGray)),
+                    const SizedBox(height: 4),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        FavoriteButtonWidget(
+                          isFavorited: false,
+                          onPressed: () {
+                            print('click favorite');
+                          },
+                          postUid: postModel.postUid,
+                        ),
+                        Text(postModel.favoriteList.length.toString(), style: const TextStyle(fontSize: 16)),
+                        const SizedBox(width: 8),
+                        const Icon(Icons.article_outlined, size: 16),
+                        const SizedBox(width: 4),
+                        Text(postModel.commentList.length.toString(), style: const TextStyle(fontSize: 16)),
+                      ],
+                    ),
                   ],
                 ),
               ),
-              const SizedBox(width: 5,),
-              Row(
-                children: [
-                  const Icon(Icons.article_outlined,size: 16,),
-                  Text(postModel.commentList.length.toString(),style: const TextStyle(fontSize: 16),)
-                ],
-              ),
-            ],
-          )
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
