10bddc6ec7fd42642c81bdb9a667752afb6e5da6
[APP1] 고규화 <rhrbghk@naver.com>
2024-09-30 16:46:07 +0900

    notification 권한 작업중

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:auction/config/theme.dart';
+import 'package:auction/models/user_model.dart';
+import 'package:auction/utils/notification_handler.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:auction/route.dart';
@@ -13,40 +15,109 @@ import 'package:auction/providers/user_provider.dart';
 // firebase 패키지
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
+}
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+FlutterLocalNotificationsPlugin();
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel',
+  'High Importance Notifications',
+  description: 'This channel is used for important notifications.',
+  importance: Importance.high,
+);
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Firebase 초기화
+  await Firebase.initializeApp();
+
+
+  // Firebase Messaging 설정
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  final notificationHandler = NotificationHandler();
+  await notificationHandler.initialize();
 
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-            create: (context) => ThemeProvider()), //테마 변경 Provider
-        ChangeNotifierProvider(
-            create: (context) => PostProvider()), //포스트 Provider
-        ChangeNotifierProvider(
-            create: (context) => ChatProvider()), // 채팅 Provider
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+        ChangeNotifierProvider(create: (context) => PostProvider()),
+        ChangeNotifierProvider(create: (context) => ChatProvider()),
         ChangeNotifierProvider(create: (_) => AuthProvider()),
         ChangeNotifierProvider(create: (context) => MyProvider()),
         ChangeNotifierProvider(create: (_) => UserProvider()),
+        Provider<NotificationHandler>.value(value: notificationHandler),
       ],
       child: MyApp(),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _initializeApp();
+  }
+
+  Future<void> _initializeApp() async {
+    final authProvider = Provider.of<AuthProvider>(context, listen: false);
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    final notificationHandler = Provider.of<NotificationHandler>(context, listen: false);
+
+    // AuthProvider가 이미 초기화되어 있으므로 추가 초기화는 필요 없음
+
+    // 현재 사용자 확인
+    UserModel? currentUser = authProvider.currentUserModel;
+
+    if (currentUser != null) {
+      // 사용자가 로그인한 상태
+      print("User is logged in: ${currentUser.nickname}");
+
+      // UserProvider를 사용하여 최신 사용자 정보 가져오기
+      await userProvider.fetchUser(currentUser.uid);
+
+      // FCM 토큰 가져오기 및 등록
+      final token = await notificationHandler.getToken();
+      if (token != null) {
+        print('FCM Token: $token');
+        // FCM 토큰을 서버에 등록하는 로직 짜야합니다
+        // 예: await authProvider.updateFCMToken(token);
+
+      }
+    } else {
+      print("No user is currently logged in");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<ThemeProvider>(
       builder: (context, themeProvider, child) {
         return MaterialApp.router(
-          theme: themeProvider.currentTheme, // 라이트 모드 테마
-          darkTheme: darkThemeData(), // 다크 모드 테마
-          themeMode: themeProvider.isDarkTheme
-              ? ThemeMode.dark
-              : ThemeMode.light, // 테마 모드 설정
+          theme: themeProvider.currentTheme,
+          darkTheme: darkThemeData(),
+          themeMode: themeProvider.isDarkTheme ? ThemeMode.dark : ThemeMode.light,
           routerDelegate: router.routerDelegate,
           routeInformationParser: router.routeInformationParser,
           routeInformationProvider: router.routeInformationProvider,
