0a0aea90596d166acf00d3ddc03e35feff537767
[APP1] 정원교 <dnjsry159@naver.com>
2024-09-30 17:46:50 +0900

    fbfunction connected

--- functions/index.js
+++ functions/index.js
@@ -9,10 +9,34 @@
 
 const functions = require("firebase-functions");
 
-exports.helloWorld = functions.region("asia-northeast3").https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+exports.helloWorld = functions.region("asia-northeast3").https.onCall((data, context) => {
+    return "Hello from Firebase!";
 });
 
+
+exports.sendPushNotification = functions.region("asia-northeast3").https.onCall(async (data, context) => {
+    // 클라이언트로부터 받은 데이터
+    const { token, title, body } = data;
+
+    // 푸시 알림 메시지 구성
+    const message = {
+        notification: {
+            title: title,
+            body: body
+        },
+        token: token
+    };
+
+    try {
+        // 푸시 알림 전송
+        const response = await admin.messaging().send(message);
+        console.log('Successfully sent message:', response);
+        return { success: true, message: "Notification sent successfully" };
+    } catch (error) {
+        console.log('Error sending message:', error);
+        throw new functions.https.HttpsError('internal', 'Error sending push notification');
+    }
+});
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
